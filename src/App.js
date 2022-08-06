84259c33730741777edb86d7ade7d9ed073dafb5
Youngwook-Jeon <ela87@naver.com>
2022-08-06 18:17:51 +0900

    Add the footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Header, Footer } from "./components";
-import { Home, Contact } from "./pages";
+import { Home, Contact, Login, Register, Reset } from "./pages";
 
 function App() {
   return (
@@ -10,6 +10,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reset" element={<Reset />} />
         </Routes>
         <Footer />
       </BrowserRouter>
